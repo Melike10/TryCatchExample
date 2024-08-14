6a6ba41c850f5355693db6fbdefa52a7f134ee72
GÖZ'SS <GZ'SS@DESKTOP-NKTDPLS>
2024-08-14 18:20:07 +0300

    Add project files.

---
+++ TryCatchExample/Program.cs
@@ -0,0 +1,19 @@
+﻿bool devam = false;
+while (!devam)
+{
+
+    try {
+        Console.Write("Bir sayı giriniz: ");
+        string str = Console.ReadLine();
+        int sayi = Convert.ToInt32(str);
+
+        devam = true;
+        Console.WriteLine("Girdiğiniz sayının karesi : "+ Math.Pow(sayi,2));
+
+    }
+    catch(Exception ex) {
+        Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+    }
+
+
+}
